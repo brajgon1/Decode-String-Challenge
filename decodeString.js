09d2f4bf60f7ec7f8a0eb891b8924451321cad7e
Brandon Gonzalez <brajgon@gmail.com>
2024-02-22 22:02:57 -0700

    decodeString.js

--- decodeString.js
+++ decodeString.js
@@ -1,1 +1,17 @@
 // Write your code below this line
+
+function decoder(code) {
+    const alpha = "abcdefghijklmnopqrstuvwxyz";
+    const num = code[0];
+    let string = code.split("");
+    let alphaArr = alpha.split("");
+    string.shift();
+    let answer = "";
+    string.forEach((letter) => {
+      let ltrIndex = alphaArr.findIndex((e) => e === letter);
+      answer += alphaArr[ltrIndex + +num];
+    });
+    return answer;
+  }
+  
+  console.log(decoder("8abcdef"));
